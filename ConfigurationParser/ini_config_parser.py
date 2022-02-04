6e21f374b2d0bdefaa29c85da3798e80e540a7b0
adwojak <dwojaka207@gmail.com>
2022-02-04 19:25:15 +0100

    Added ini config parser without required/optional checking

--- ConfigurationParser/ini_config_parser.py
+++ ConfigurationParser/ini_config_parser.py
@@ -1,6 +1,45 @@
 from configparser import ConfigParser
 
 
-class IniConfigParser:
-    def __init__(self, path):
-        pass
+class Parameter:
+    def __init__(self, name=None, required=True, default=None, cast_type=str):
+        self.name = name
+        self.required = required
+        self.default = default
+        self.cast_type = cast_type
+
+    def parse_parameter(self, parameter_value):
+        # TODO Error with casting types
+        return self.cast_type(parameter_value)
+
+
+class Section:
+    def __init__(self, name=None, required=True, **parameters):
+        self.name = name
+        self.required = required
+        self.parameters = parameters
+
+    def parse_section(self, section):
+        section_parsed = {}
+        for key, parameter in self.parameters.items():
+            # TODO Error if missing parameter
+            parameter_name = parameter.name or key
+            parameter_value = section.get(parameter_name, parameter.default)
+            if parameter_value:
+                section_parsed[key] = parameter.parse_parameter(parameter_value)
+        return section_parsed
+
+
+class IniConfigParser(ConfigParser):
+    def __init__(self, path, config_mapping, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.read(path)
+        self.parsed_config = self.parse_config(config_mapping)
+
+    def parse_config(self, config_mapping):
+        configuration = {}
+        for key, section in config_mapping.items():
+            # TODO Error if missing section
+            section_name = section.name or key
+            configuration[key] = section.parse_section(self[section_name])
+        return configuration
--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-from ConfigurationParser.ini_config_parser import IniConfigParser
+from ConfigurationParser.ini_config_parser import IniConfigParser, Section, Parameter
+
+
+config_mapping = {
+    "settings": Section(
+        "SETTINGS",
+        status_port=Parameter("StatusPort", cast_type=int),
+        archive=Parameter("Archive", cast_type=int),
+        log_file=Parameter("LogFile"),
+        version=Parameter("Version"),
+        optional=Parameter("Optional", required=False, default="Optional value"),
+        second_optional=Parameter(required=False),
+        camel_case_param=Parameter(),
+    )
+}
 
 
 if __name__ == "__main__":
     # For testing purposes
-    config = IniConfigParser("ConfigurationParser/configuration.ini")
+    config = IniConfigParser("ConfigurationParser/configuration.ini", config_mapping).parsed_config
+    print(config)
