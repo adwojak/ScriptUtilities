03cd7bb5a54158a72ba7478497d6fb0249396896
adwojak <dwojaka207@gmail.com>
2022-02-06 17:27:28 +0100

    Added list parsing and test

--- ConfigurationParser/ini_config_parser.py
+++ ConfigurationParser/ini_config_parser.py
@@ -1,16 +1,26 @@
 from configparser import ConfigParser
+from json import loads, JSONDecodeError
 
 
 class Parameter:
-    def __init__(self, name=None, required=True, default=None, cast_type=str):
+    def __init__(self, name=None, required=True, default=None, cast_type=str, delimiter=","):
         self.name = name
         self.required = required
         self.default = default
         self.cast_type = cast_type
+        self.delimiter = delimiter
 
     def parse_parameter(self, parameter_value):
         # TODO Error with casting types
+        if self.cast_type == list:
+            return self.cast_to_list(parameter_value)
         return self.cast_type(parameter_value)
+
+    def cast_to_list(self, parameter_value):
+        try:
+            return loads(parameter_value)
+        except JSONDecodeError:
+            return parameter_value.split(self.delimiter)
 
 
 class Section:
--- ConfigurationParser/tests/test_ini_config_parser.py
+++ ConfigurationParser/tests/test_ini_config_parser.py
@@ -3,7 +3,34 @@ from ConfigurationParser.ini_config_parser import IniConfigParser, Section, Para
 
 class TestIniConfigParser:
     def test_success(self):
-        pass
+        mapping = {
+            "required_section": Section(
+                "REQUIRED_SECTION",
+                string_value=Parameter(name="StringValue"),
+                int_value=Parameter(name="IntValue", cast_type=int),
+                bool_value=Parameter(name="BoolValue", cast_type=bool),
+                example_list=Parameter(name="ExampleList", cast_type=list, delimiter=None),
+                example_list2=Parameter(name="ExampleList2", cast_type=list),
+                example_list3=Parameter(name="ExampleList3", cast_type=list),
+                example_list4=Parameter(name="ExampleList4", cast_type=list, delimiter=";"),
+                camel_case_value=Parameter(),
+                optional_value=Parameter(name="OptionalValue", default="Optional"),
+            )
+        }
+        config = IniConfigParser("ConfigurationParser/tests/test_config.ini", mapping).parsed_config
+        assert config == {
+            "required_section": {
+                "string_value": "Some string",
+                "int_value": 1337,
+                "bool_value": True,
+                "example_list": ["123", "332"],
+                "example_list2": ["test", "test2"],
+                "example_list3": [6, 4],
+                "example_list4": ["test1", "test2"],
+                "camel_case_value": "Some value",
+                "optional_value": "Optional",
+            }
+        }
 
     def test_missing_section(self):
         pass
