3173489bee6f5c0cb49d8b8843716cc14bca1afd
adwojak <dwojaka207@gmail.com>
2022-02-06 11:50:39 +0100

    Tests structure for ini config parser

---
+++ ConfigurationParser/tests/test_ini_config_parser.py
@@ -0,0 +1,21 @@
+from ConfigurationParser.ini_config_parser import IniConfigParser, Section, Parameter
+
+
+class TestIniConfigParser:
+    def test_success(self):
+        pass
+
+    def test_missing_section(self):
+        pass
+
+    def test_missing_parameter(self):
+        pass
+
+    def test_error_with_casting_type(self):
+        pass
+
+    def test_default_values(self):
+        pass
+
+    def test_list_parsing(self):
+        pass
