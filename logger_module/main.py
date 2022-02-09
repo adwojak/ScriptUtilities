d9917b2b967f6ea07add28add3a9c6206c996bad
adwojak <dwojaka207@gmail.com>
2022-02-09 11:33:29 +0100

    Logger configuration

--- logger_module/main.py
+++ logger_module/main.py
@@ -1,2 +1,14 @@
-from logging import getLogger
+import logging
 
+
+LOG_FILE_NAME = "logger_file.log"
+LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+LOG_LEVEL = "INFO"
+
+
+def setup_logger():
+    logging.basicConfig(filename=LOG_FILE_NAME, format=LOG_FORMAT, level=LOG_LEVEL)
+
+
+if __name__ == "__main__":
+    setup_logger()
